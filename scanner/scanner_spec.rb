1ba0868d1f1244a44ed70237cc6bcfe27e8894b4
Bala Paranj <bparanj@gmail.com>
2012-05-17 10:34:31 -0700

    Solution that uses mock instead of fake object

--- scanner/scanner_spec.rb
+++ scanner/scanner_spec.rb
@@ -4,10 +4,9 @@ require_relative 'fake_display'
 describe Scanner do
     
   it "should scan and display the name and price of the scanned item on a cash register display" do
-    fake_display = FakeDisplay.new
+    fake_display = mock
+    fake_display.should_receive(:display).with("Milk $3.99")
     scanner = Scanner.new(fake_display)
     scanner.scan("1")
-    
-    fake_display.last_line_item.should == "Milk $3.99"
   end
 end
