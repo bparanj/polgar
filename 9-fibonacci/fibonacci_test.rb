12f5d1b8f1d28a4ac3d11dde8fe56a09b7ade333
Bala Paranj <bparanj@gmail.com>
2012-05-19 08:29:29 -0700

    Broken test forced the implementation to change. Dirty implementation passes the test.

--- 9-fibonacci/fibonacci_test.rb
+++ 9-fibonacci/fibonacci_test.rb
@@ -1,11 +1,14 @@
 require 'test/unit'
 
-# Version 4
-# made fib(1) = 1 pass very quickly using a dirty implementation
-
+# version 5
+# Broken test forced the implementation to change. Dirty implementation passes the test.
 class Fibonacci
   def self.of(number)
-    number
+    if number == 2
+      return 1
+    else
+      return number
+    end
   end
 end
 
@@ -18,5 +21,10 @@ class FibonacciTest < Test::Unit::TestCase
   def test_fibonacci_of_one_is_one
     fib_of_one = Fibonacci.of(1)
     assert_equal(1, fib_of_one)
+  end
+  
+  def test_fibonacci_of_two_is_one
+    fib_of_two = Fibonacci.of(2)
+    assert_equal(1, fib_of_two)
   end
 end
