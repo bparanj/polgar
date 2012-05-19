e783e189b4c4565f2ad8d3d569478b3da627216b
Bala Paranj <bparanj@gmail.com>
2012-05-19 08:15:42 -0700

    Added fibonacci version 1 solution

---
+++ 9-fibonacci/fibonacci_test.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+
+# Version 1
+# Got proper require to execute the test. Proper naming of test following naming convention.
+class FibonacciTest < Test::Unit::TestCase
+  # test_method_under_test_scenario_expected_behavior
+  def test_fibonacci_of_zero_is_zero
+    fail "fail"
+  end
+end
