194c5670d0829863cb46f34866a425029cd24382
Bala Paranj <bparanj@gmail.com>
2012-05-19 08:26:53 -0700

    Don't change the test code and code under test at the same time

--- 9-fibonacci/fibonacci_test.rb
+++ 9-fibonacci/fibonacci_test.rb
@@ -1,16 +1,20 @@
 require 'test/unit'
 
+# version 3
+# Found the right assertion to use. Overcame the temptation to change the test code
+# and code under test at the same time. Thereby test driving the development of the 
+# production code. Got the test to pass quickly  
+# by using a fake implementation. The implementation returns a constant
+ 
 class Fibonacci
   def self.of(number)
     0
   end
 end
 
-# Version 2
-# Discovery of public API
 class FibonacciTest < Test::Unit::TestCase
   def test_fibonacci_of_zero_is_zero
     fib_of_zero = Fibonacci.of(0)
-    assert_equal(fib_of_zero, 0)
+    assert_equal(0, fib_of_zero)
   end
 end
