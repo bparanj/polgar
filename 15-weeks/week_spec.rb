b0d88da91187bc13ecc04ee7d7bcd9b37adaa458
Bala Paranj <bparanj@gmail.com>
2012-05-20 07:16:39 -0700

    Added three contract tests that explictly documents the behavior of the API for invalid inputs. Hash#fetch throws exception that is implicit in the code.

--- 15-weeks/week_spec.rb
+++ 15-weeks/week_spec.rb
@@ -15,6 +15,13 @@ class Week
     end
   end
   
+  def self.end(n)
+    if n.to_i < 5
+      raise "The given number is not a weekend"
+    else
+      fetch(n)
+    end
+  end
 end
 
 describe Week do
@@ -24,10 +31,22 @@ describe Week do
     
     day.should == :monday
   end
-  
+  # contract test
   it "should return false for numbers that does not correspond to week day" do
     day = Week.day("7")
     
     day.should be_false
+  end
+  # contract test  
+  it "should throw exception for numbers that does not correspond to week end" do
+    expect do
+      week_end = Week.end("4")
+    end.to raise_error
+  end
+  # contract test
+  it "should throw exception for numbers that is out of range" do
+    expect do
+      week_end = Week.end("40")
+    end.to raise_error    
   end
 end
