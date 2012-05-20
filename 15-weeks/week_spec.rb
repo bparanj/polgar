e7bc270845c1164534c4f9e56f0bde265e0a00fc
Bala Paranj <bparanj@gmail.com>
2012-05-20 06:49:54 -0700

    Contract test, first version that passes when return value is checked for false

---
+++ 15-weeks/week_spec.rb
@@ -0,0 +1,33 @@
+class Week
+  DAYS = { "1" =>  :monday, 
+           "2" =>  :tuesday, 
+           "3" =>  :wednesday, 
+           "4" =>  :thursday, 
+           "5" =>  :friday,
+           "6" =>  :saturday,
+           "7" =>  :sunday}
+  
+  def self.day(n)
+    if n.to_i < 6
+      DAYS[n] 
+    else
+      nil
+    end
+  end
+  
+end
+
+describe Week do
+  
+  it "should return monday as the first day of the week" do
+    day = Week.day("1")
+    
+    day.should == :monday
+  end
+  
+  it "should return false for numbers that does not correspond to week day" do
+    day = Week.day("7")
+    
+    day.should be_false
+  end
+end
