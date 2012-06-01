50cd333d53274b19c75397ccaed2ada0a4adb773
Bala Paranj <bparanj@gmail.com>
2012-05-31 17:16:11 -0700

    Fixed abuse of mocks

--- 20-tweet_analysis/tweet_analyser_spec.rb
+++ 20-tweet_analysis/tweet_analyser_spec.rb
@@ -20,7 +20,10 @@ end
 describe TweetAnalyzer do
   it "finds the frequency of words in a user's tweets" do
     user = double('user')
-    analyzer = TweetAnalyzer.new(user)
+    expected_tweets = ["one two", "two"]
+    user.stub(:recent_tweets).and_return expected_tweets
+    
+    analyzer = TweetAnalyzer.new(user)    
     analyzer.word_frequency
 
     analyzer.histogram("one").should == 1
@@ -28,11 +31,12 @@ describe TweetAnalyzer do
   
   it "asks the user for recent tweets" do
     user = double('user')
-    analyzer = TweetAnalyzer.new(user)
     expected_tweets = ["one two", "two"]
-    user.should_receive(:recent_tweets).and_return expected_tweets
+    user.stub(:recent_tweets).and_return expected_tweets
     
-    histogram = analyzer.word_frequency
+    analyzer = TweetAnalyzer.new(user)
+    analyzer.word_frequency
+    
     analyzer.histogram("two").should == 2
   end
 end
