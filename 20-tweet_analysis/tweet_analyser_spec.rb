5bfbdaf38037bcccb77b3338bec0e77e40629e02
Bala Paranj <bparanj@gmail.com>
2012-05-31 17:18:57 -0700

    Extracted common setup to before each method

--- 20-tweet_analysis/tweet_analyser_spec.rb
+++ 20-tweet_analysis/tweet_analyser_spec.rb
@@ -18,23 +18,21 @@ class TweetAnalyzer
 end
 
 describe TweetAnalyzer do
-  it "finds the frequency of words in a user's tweets" do
-    user = double('user')
+  before(:each) do
+    @user = double('user')
     expected_tweets = ["one two", "two"]
-    user.stub(:recent_tweets).and_return expected_tweets
-    
-    analyzer = TweetAnalyzer.new(user)    
+    @user.stub(:recent_tweets).and_return expected_tweets
+  end
+  
+  it "finds the frequency of words in a user's tweets" do    
+    analyzer = TweetAnalyzer.new(@user)    
     analyzer.word_frequency
 
     analyzer.histogram("one").should == 1
   end
   
-  it "asks the user for recent tweets" do
-    user = double('user')
-    expected_tweets = ["one two", "two"]
-    user.stub(:recent_tweets).and_return expected_tweets
-    
-    analyzer = TweetAnalyzer.new(user)
+  it "asks the user for recent tweets" do    
+    analyzer = TweetAnalyzer.new(@user)
     analyzer.word_frequency
     
     analyzer.histogram("two").should == 2
