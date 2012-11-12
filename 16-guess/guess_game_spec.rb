4bf936cc514845745c6a661b97a5cab8c4984111
Zepho <zepho@zepho-mac-pro.local>
2012-11-12 15:18:01 -0800

    Deleted duplicate test

--- 16-guess/guess_game_spec.rb
+++ 16-guess/guess_game_spec.rb
@@ -2,21 +2,18 @@ require_relative 'guess_game'
 
 describe GuessGame do
     
-  it "should display 'Welcome to the Guessing Game' when the game begins" do
+  it "should display 'Welcome to the Guessing Game' to the standard output when the game begins" do
     fake_console = double('Console').as_null_object
     fake_console.should_receive(:output).with('Welcome to the Guessing Game')
+  
     game = GuessGame.new(fake_console)
     game.start
   end
-  
-  it "should display 'Welcome to the Guessing Game' to the standard output when the game begins" do
-    game = GuessGame.new
-    game.start
-  end
-  
+    
   it "should prompt the user to enter the number representing their guess. Prompt should explain to users what they are to do." do
     fake_console = double('Console').as_null_object
     fake_console.should_receive(:prompt).with('Enter a number between 1 and 100 to guess the number')
+  
     game = GuessGame.new(fake_console)
     game.start    
   end
@@ -24,6 +21,7 @@ describe GuessGame do
   it "should perform validation of the guess entered by the user : lower than 1" do
     fake_console = double('Console').as_null_object
     fake_console.stub(:input) { 0 }
+  
     game = GuessGame.new(fake_console)
     game.get_user_guess
     
@@ -41,6 +39,7 @@ describe GuessGame do
     fake_randomizer = stub(:get => 25)
     fake_console = double('Console').as_null_object
     fake_console.should_receive(:output).with('Your guess is lower')
+  
     game = GuessGame.new(fake_console, fake_randomizer)
     game.stub(:random).and_return { 25 }
     game.guess = 10    
@@ -50,6 +49,7 @@ describe GuessGame do
     fake_randomizer = stub(:get => 25)
     fake_console = double('Console').as_null_object
     fake_console.should_receive(:output).with('Your guess is higher')
+  
     game = GuessGame.new(fake_console, fake_randomizer)
     game.guess = 35    
   end
@@ -58,6 +58,7 @@ describe GuessGame do
     fake_randomizer = stub(:get => 25)
     fake_console = double('Console').as_null_object
     fake_console.should_receive(:output).with('Your guess is correct')
+  
     game = GuessGame.new(fake_console, fake_randomizer)
     game.guess = 25
   end
